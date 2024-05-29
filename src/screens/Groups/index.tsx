586d3c4d1c986ecd2f2ee0d3c7d7fa0b451ebe39
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-05-28 23:20:41 -0300

    Criando Componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
@@ -8,6 +9,9 @@ export function Groups() {
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera do Ignite" />
+      <GroupCard title="Galera do Explorer" />
     </Container>
   );
 }
