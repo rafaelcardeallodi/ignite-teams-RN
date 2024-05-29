4e92a1c71f59372685eb896daef0254d0ab601f3
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-05-29 15:38:55 -0300

    Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,31 @@
-import { Header } from '@components/Header';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Container } from './styles';
+import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
+  const [groups, setGroups] = useState(['Galera do Ignite', 'Galera do Explorer'])
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
-      <GroupCard title="Galera do Explorer" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => {
+          return(
+            <GroupCard 
+              title={item}
+            />
+          )
+        }}
+      />
     </Container>
   );
 }
