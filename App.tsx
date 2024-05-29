9f4849d0b8186739010f96e921386e2505d935d3
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-05-29 16:23:17 -0300

    Criando Interface NewGroup

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { defaultTheme } from './src/theme';
 
-import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 import { Loading } from '@components/Loading';
 
 export default function App() {
@@ -17,7 +17,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <Groups/> : <Loading/> }
+        { fontsLoaded ? <NewGroup /> : <Loading /> }
     </ThemeProvider>
   );
 }
---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,23 @@
+import { Header } from "@components/Header";
+import { Container, Content, Icon } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+
+export function NewGroup(){
+  return (
+    <Container>
+      <Header showBackButton />
+      
+      <Content>
+        <Icon />
+        
+        <Highlight 
+          title="Nova turma" 
+          subtitle="Crie uma nova turma para adicionar as pessoas" 
+        />
+
+        <Button>Criar</Button>
+      </Content>
+    </Container>
+  )
+}
